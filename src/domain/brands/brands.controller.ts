7772e38ebde9ca4a88a1a0a391098c655378775d
Vitaliy Zavgorodniy <vince.kkr@gmail.com>
2023-08-29 17:05:14 +0200

    added injection for brands

--- src/domain/brands/brands.controller.ts
+++ src/domain/brands/brands.controller.ts
@@ -7,28 +7,28 @@ import { CreateBrandDto } from './dto/create-brand.dto';
 
 @Controller('brands')
 export class BrandsController {
-  constructor(private service: BrandsService) {}
+  constructor(private brandsService: BrandsService) {}
 
   @Get('')
   public async getAllBrands() {
-    return await this.service.getAll();
+    return await this.brandsService.getAll();
   }
 
   @Get('/:id')
   public async getBrandById(@Param('id') id: string) {
-    return await this.service.getById(id);
+    return await this.brandsService.getById(id);
   }
 
   @Post('')
   public async createBrand(dto: CreateBrandDto) {
-    const brand = await this.service.create(dto);
+    const brand = await this.brandsService.create(dto);
 
     return brand;
   }
 
   @Delete('/:id')
   public async deleteBrand(@Param('id') id: string) {
-    const deletedBrand = await this.service.delete(id);
+    const deletedBrand = await this.brandsService.delete(id);
 
     return deletedBrand;
   }
--- src/domain/brands/brands.module.ts
+++ src/domain/brands/brands.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { BrandsController } from './brands.controller';
 import { BrandsService } from './brands.service';
 
+import { Brand, BrandSchema } from './schemas/brand.schema';
+
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Brand.name, schema: BrandSchema }]),
+  ],
   controllers: [BrandsController],
-  providers: [BrandsService]
+  providers: [BrandsService],
 })
 export class BrandsModule {}
